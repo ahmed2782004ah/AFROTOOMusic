e1c482887065641420a2e75b8cc7b598bcffa498
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-14 04:50:44 +0200

    Update hmsa.py

--- AFROTOMusic/plugins/play/hmsa.py
+++ AFROTOMusic/plugins/play/hmsa.py
@@ -8,37 +8,37 @@ from config import *
 
 hmses = {}
 
-@app.on_message(filters.reply & filters.command("همسه","اهمس", "") & filters.group, group=7881108771)
-def reply_with_link(client, message):
+@app.on_message(filters.reply & filters.regex("همسه") & filters.group, group=579)
+async def reply_with_link(client, message):
     user_id = message.reply_to_message.from_user.id
     my_id = message.from_user.id
     bar_id = message.chat.id
-    start_link = f"https://t.me/{(app.get_me()).username}?start=hms{my_id}to{user_id}in{bar_id}"
+    start_link = f"https://t.me/{app.username}?start=hms{my_id}to{user_id}in{bar_id}"
     reply_markup = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton(" اضغط لإرسال الهمسه!", url=start_link)]
+            [InlineKeyboardButton("- اضغط لإرسال الهمسه!", url=start_link)]
         ]
-    )
-    message.reply_text("\n⬡ إضغط لإرسال همسه!\n", reply_markup=reply_markup)
+    )  
+    await message.reply_text("\n╢ إضغط لإرسال همسه!\n", reply_markup=reply_markup)
 
 waiting_for_hms = False
-
-@app.on_message(filters.command("start"), group=7880987111)
-def hms_start(client, message):
+@app.on_message(filters.command("start"), group=5790)
+async def hms_start(client, message):
   
   if message.text.split(" ", 1)[-1].startswith("hms"):
     global waiting_for_hms, hms_ids
     hms_ids = message.text
     waiting_for_hms = True
-    message.reply_text(
-      "-> أرسل الهمسه الآن.\n√",
+    await message.reply_text(
+      "-> أرسل الهمسه الآن.\n√", 
       reply_markup = InlineKeyboardMarkup ([[
-        InlineKeyboardButton ("إلغاء ", callback_data="hms_cancel")
+        InlineKeyboardButton ("إلغاء ❌️", callback_data="hms_cancel")
       ]])
     )
+    return
 
-@app.on_message(filters.private & filters.text & ~filters.command("start"), group=788122345111)
-def send_hms(client, message):
+@app.on_message(filters.private & filters.text & ~filters.command("start"), group=576)
+async def send_hms(client, message):
   
   global waiting_for_hms
   if waiting_for_hms:    
@@ -50,17 +50,21 @@ def send_hms(client, message):
     
     hmses[str(to_id)] = { "hms" : message.text, "bar" : in_id }
     
-    message.reply_text("-> تم ارسال الهمسه.\n√")
+    await message.reply_text("-> تم ارسال الهمسه.\n√")
     
-    app.send_message(
-      chat_id = in_id,
-      text = f"╖ المستخدم [{app.get_chat(to_id).first_name}]({to_url})\n⬡ لديك همسه من البني آدم دا [{app.get_chat(from_id).first_name}]({from_url})\n╜انت فقط من يستطيع رؤيتها ",
-      reply_markup = InlineKeyboardMarkup ([[InlineKeyboardButton("- اضغط لرؤية الهمسه 👀", callback_data = "hms_answer")]])
-    )
+    await app.send_message(
+      chat_id = in_id, 
+      text = f"تم استلام همسه جديده ✨♥\nلروئيه الهمسه في الزر بالاسفل ✨♥\nفقط المرسل اليه الهمسه هو الي يقدر يشوفها 🔐",
+      reply_markup = InlineKeyboardMarkup ([[
+        InlineKeyboardButton("- اضغط لرؤية الهمسه 🥺", callback_data = "hms_answer"), 
+     ],[InlineKeyboardButton("مستلم الهمسه✨♥", url=f"tg://openmessage?user_id={to_id}")
+     ],[InlineKeyboardButton("مرسل الهمسه✨♥", url=f"{from_url}")]])
+      
+     ) 
     
     waiting_for_hms = False
   
-@app.on_callback_query(filters.regex("hms_answer"), group=7881334211)
+@app.on_callback_query(filters.regex("hms_answer"), group=5766565)
 def display_hms(client, callback):
   
   in_id = callback.message.chat.id
@@ -72,8 +76,8 @@ def display_hms(client, callback):
   else:
     callback.answer( "بطل لعب ف حاجه مش بتاعتك يابابا 🗿", show_alert = True )
     
-@app.on_callback_query(filters.regex("hms_cancel"), group=788166511)
-def display_hms(client, callback):
+@app.on_callback_query(filters.regex("hms_cancel"), group=57967)
+def cancel_hms(client, callback):
   
   global waiting_for_hms
   waiting_for_hms = False
