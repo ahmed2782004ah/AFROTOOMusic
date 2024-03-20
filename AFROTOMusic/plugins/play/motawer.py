722b301f9f2f16c00a50b82f862ef10b74d450b6
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 00:54:10 +0200

    Update motawer.py

--- AFROTOMusic/plugins/play/motawer.py
+++ AFROTOMusic/plugins/play/motawer.py
@@ -10,15 +10,14 @@ from AFROTOMusic import app
 from config import OWNER_ID, LOGGER_ID
 
 
-@app.on_message(command(["مطور", "المطور"]) & filters.group)
-async def zilzal(client: Client, message: Message):
+@app.on_message(command(["مطور البوت", "مطور", "المطور"]))
+async def zdatsr(client: Client, message: Message):
     usr = await client.get_users(OWNER_ID)
     name = usr.first_name
     usrnam = usr.username
-    photo = user.photo.big_file_id
-    photo = await client.download_media(photo)
-    async for photo in client.iter_profile_photos(OWNER_ID, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""ٴ<b>•────‌‌‏✯ ʑ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/ee99f5b99514304efeffd.jpg",
+        caption=f"""ٴ<b>•────‌‌‏✯ ʑ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
                     
 - 𝚆𝙾𝙽𝙴𝚁 :[{usr.first_name}](https://t.me/{OWNER})
 - 𝚄𝚂𝙴𝚁 :@{usrnam} 
@@ -30,14 +29,8 @@ reply_markup=InlineKeyboardMarkup(
             [            
               InlineKeyboardButton (name, url=f"https://t.me/{usrnam}"),
             ],[
-              InlineKeyboardButton("•✯ ᯓ 「𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾」، ⦃𓏛 ✯•", url="https://t.me/T_Y_E_X"),
+              InlineKeyboardButton("•✯ ᴢᴛʜᴏɴ_ᴍᴜsɪᴄ ✯•", url="https://t.me/Zelzal_Music"),
             ],
           ]
        )                 
     )                    
-                    sender_id = message.from_user.id
-                    sender_name = message.from_user.first_name
-                    senderuser = message.from_user.username
-                    sender_user = "@{senderuser}" if senderuser else "لا يوجـد"
-                    await app.send_message(OWNER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
-                    return await app.send_message(LOGGER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
