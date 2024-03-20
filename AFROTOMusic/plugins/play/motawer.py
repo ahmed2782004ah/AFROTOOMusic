448e934367fc4cf1fa7958bcb001882c23922fd1
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 00:48:32 +0200

    Update motawer.py

--- AFROTOMusic/plugins/play/motawer.py
+++ AFROTOMusic/plugins/play/motawer.py
@@ -16,7 +16,9 @@ async def zilzal(client: Client, message: Message):
     name = usr.first_name
     usrnam = usr.username
     photo = user.photo.big_file_id
-     photo = await client.download_media(photo)       caption=f"""ٴ<b>•────‌‌‏✯ ʑ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
+    photo = await client.download_media(photo)
+    async for photo in client.iter_profile_photos(OWNER_ID, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""ٴ<b>•────‌‌‏✯ ʑ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
                     
 - 𝚆𝙾𝙽𝙴𝚁 :[{usr.first_name}](https://t.me/{OWNER})
 - 𝚄𝚂𝙴𝚁 :@{usrnam} 
@@ -33,3 +35,9 @@ reply_markup=InlineKeyboardMarkup(
           ]
        )                 
     )                    
+                    sender_id = message.from_user.id
+                    sender_name = message.from_user.first_name
+                    senderuser = message.from_user.username
+                    sender_user = "@{senderuser}" if senderuser else "لا يوجـد"
+                    await app.send_message(OWNER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
+                    return await app.send_message(LOGGER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
