1d521a8f7cb9998e2407dbd532fc88cbe67c11bf
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 02:33:09 +0200

    Update sora.py

--- AFROTOMusic/plugins/play/sora.py
+++ AFROTOMusic/plugins/play/sora.py
@@ -21,18 +21,17 @@ async def ihd(client: Client, message: Message):
         )
                            )
 
-@app.on_message(command([f"استوريهات", "استوري"]))
-async def ihd(client: Client, message: Message):
-    rl = random.randint(2,50)
-    url = f"https://t.me/yoipopl{rl}"
-    await client.send_audio(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاستوري  لـك",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-                           )
+listmu = []
+@Client.on_message(filters.command(["اغاني", "غنيلي", "غ", "اغنيه","اغنية عشوائية"], ""))
+async def voece(client, message):
+  if not message.chat.type == enums.ChatType.PRIVATE:
+    await joinch(message)
+  if len(listmu) == 0:
+   user = await get_userbot(client.me.username)
+   async for msg in user.get_chat_history("ELNQYBMUSIC"):
+      if msg.media:
+        listmu.append(msg.id)
+  audi = random.choice(listmu)
+  audio = f"https://t.me/ELNQYBMUSIC/{audi}"
+  await message.reply_audio(audio=audio, caption="**♪ 𝑱𝒐𝒊𝒏 ➧ @UI_VM  💎 .**")
     
