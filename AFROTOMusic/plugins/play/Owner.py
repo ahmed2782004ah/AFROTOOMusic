17aa7deb37f446f0ef8130695a5adfe88ea4558d
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 03:18:17 +0200

    Update Owner.py

--- AFROTOMusic/plugins/play/Owner.py
+++ AFROTOMusic/plugins/play/Owner.py
@@ -13,20 +13,20 @@ from asyncio import gather
 from pyrogram.errors import FloodWait
 
 @app.on_message(command(["المالك", "صاحب الخرابه", "المنشي"]) & filters.group)
-async def vgdg(client: Client, message: Message):
+async def gak_owne(client: Client, message: Message):
       if len(message.command) >= 2:
          return 
       else:
             chat_id = message.chat.id
             f = "administrators"
-            async for member in client.get_chat_members(chat_id, filter=f):
+            async for member in client.iter_chat_members(chat_id, filter=f):
                if member.status == "creator":
-                 id = member.usr.id
-                 key = InlineKeyboardMarkup([[InlineKeyboardButton(member.usr.first_name, usr_id=id)]])
+                 id = member.user.id
+                 key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
                  m = await client.get_chat(id)
                  if m.photo:
                        photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.usrnam}\n🎃 ¦𝙸𝙳 :{m.id}\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :{message.chat.id}",reply_markup=key)
+                       return await message.reply_photo(photo, caption=f"🕷 ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🐉 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🐰 ¦𝙸𝙳 :`{m.id}`\n🎬 ¦𝙱𝙸𝙾 :{m.bio}\n💎 ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n🗿 ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
                  else:
                     return await message.reply("• " + member.user.mention)
                     
