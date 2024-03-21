05de2a52bc7c7f4aa4b39e33a7d10f39e0a45ac0
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 04:50:50 +0200

    Update المالك.py

--- AFROTOMusic/plugins/play/المالك.py
+++ AFROTOMusic/plugins/play/المالك.py
@@ -16,7 +16,7 @@ from pyrogram.errors import FloodWait
 
 
 
-@app.on_message(command(["المالك", "صاحب الخرابه", "المنشي"]))
+@app.on_message(command(["المالك", "صاحب الخرابه", "المنشي"]) & filters.group)
 async def gak_owne(client: Client, message: Message):
       if len(message.command) >= 2:
          return 
@@ -24,13 +24,13 @@ async def gak_owne(client: Client, message: Message):
             chat_id = message.chat.id
             f = "administrators"
             async for member in client.iter_chat_members(chat_id, filter=f):
-               if member.status == "creator":
+               if member.status = "creator":
                  id = member.user.id
                  key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
                  m = await client.get_chat(id)
                  if m.photo:
                        photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"🕷 ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🐉 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🐰 ¦𝙸𝙳 :`{m.id}`\n🎬 ¦𝙱𝙸𝙾 :{m.bio}\n💎 ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n🗿 ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
+                       return await message.reply_photo(photo, caption=f"",reply_markup=key)
                  else:
                     return await message.reply("• " + member.user.mention)
                     
