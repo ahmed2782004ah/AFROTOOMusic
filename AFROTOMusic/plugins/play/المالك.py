8a02fa3e04efdfeaab244a9bf4e6576a3db3043e
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 03:47:39 +0200

    Update المالك.py

--- AFROTOMusic/plugins/play/المالك.py
+++ AFROTOMusic/plugins/play/المالك.py
@@ -1,1 +1,47 @@
+import asyncio
+import os
+import requests
+import pyrogram
+from pyrogram import Client, filters, emoji
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.errors import MessageNotModified
+from AFROTOMusic import app
+from config import OWNER_ID, LOGGER_ID
 
+
+@app.on_message(command(["مطور البوت", "مطور", "المطور"]))
+async def zdatsr(client: Client, message: Message):
+    usr = await client.get_users(creator)
+    name = usr.first_name
+    usrnam = usr.username
+    photo = usr.photo.big_file_id
+    photo = await client.download_media(photo)
+    link = f"https://t.me/{message.chat.username}"
+    title = message.chat.title if message.chat.title else message.chat.first_name
+    chat_title = f"User : {message.from_user.mention} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
+    try:
+     await client.send_message(usrnam, f"**هناك شخص بالحاجه اليك عزيزي المطور الأساسي**\n{chat_title}\nChat Id : `{message.chat.id}`",
+     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
+    except:
+        pass
+    await message.reply_photo(
+        photo=photo,
+        caption=f"""ٴ<b>•─‌‌‏ 𝚂𝙾𝚞𝚁𝙲𝙴 𝙰𝙵𝚁𝙾𝚃𝙾𝙾 ‌─‏─•</b>
+                    
+- o𝚆𝙽𝙴𝚁 :[{name}]
+- 𝚄𝚂𝙴𝚁 :@{usrnam} 
+- 𝙸𝙳 :`{usr.id}`
+ 
+ٴ<b>•──‌‌𝚂𝙾𝚞𝚁𝙲𝙴 𝙰𝙵𝚁𝙾𝚃𝙾𝙾 ──‌‌‏─‌•</b> """, 
+reply_markup=InlineKeyboardMarkup(
+          [               
+            [            
+              InlineKeyboardButton (name, url=f"https://t.me/{usrnam}"),
+            ],[
+              InlineKeyboardButton(" 𝚂𝙾𝚞𝚁𝙲𝙴 𝙰𝙵𝚁𝙾𝚃𝙾𝙾 ‌", url="https://t.me/UI_VM"),
+            ],
+          ]
+       )                 
+    )                    
+                    
