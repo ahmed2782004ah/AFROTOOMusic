206e687e479cd8ee278534ac7a83bca8ca625b3d
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 16:48:55 +0200

    Update ttt.py

--- AFROTOMusic/plugins/play/ttt.py
+++ AFROTOMusic/plugins/play/ttt.py
@@ -35,12 +35,12 @@ def get_file_id(msg: Message):
 
 @app.on_message(command(["تخ"]) & filters.group)
 async def huhh(client, message):
-    to_id = int(ahmed.split("to")[-1].split("in")[0])
-    from_id = int(ahmed.split("ahmed")[-1].split("to")[0])
+    to_id = int(caption.split("to")[-1].split("in")[0])
+    from_id = int(caption.split("caption")[-1].split("to")[0])
     in_id = int(caption.split("in")[-1])
     to_url = f"tg://openmessage?user_id={to_id}"
     from_url = f"tg://openmessage?user_id={from_id}"
-    ahmed = message.text
+    caption = message.text
     await message.reply_animation(
         animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
         caption=f"""↯︙قتل ↫ ⦗ {app.get_chat(to_id).first_name}]({to_url}) ⦘\nالضحيه دا 😢 ↫ ⦗ [{app.get_chat(from_id).first_name}]({from_url}) ⦘\nانا لله وانـا اليـه راجعـون 😢😢""",
