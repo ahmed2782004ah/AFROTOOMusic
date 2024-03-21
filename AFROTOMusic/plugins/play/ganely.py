ab6167431f495f711f3c679901fb72c1a399570b
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 02:38:53 +0200

    Update ganely.py

--- AFROTOMusic/plugins/play/ganely.py
+++ AFROTOMusic/plugins/play/ganely.py
@@ -1,1 +1,31 @@
+import asyncio
 
+import os
+import time
+import requests
+from config import START_video_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from YukkiMusic import app
+from random import  choice, randint
+
+
+
+@app.on_message(command(["غنيلي", "غني", "غنيلي. 🎙", "غنيي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,90)
+    url = f"https://t.me/gukygn/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🐉 ¦ تـم اختيـار الاغـنـية لـك",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
