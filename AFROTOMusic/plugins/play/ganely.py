489bec83104b0f0fcaa71f1a6ca173500f059aab
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 02:53:56 +0200

    Update ganely.py

--- AFROTOMusic/plugins/play/ganely.py
+++ AFROTOMusic/plugins/play/ganely.py
@@ -10,7 +10,7 @@ import random
 async def ihd(client: Client, message: Message):
     rl = random.randint(2,50)
     url = f"https://t.me/gukygn/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الصوره لـك",
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار اغنيه لـك",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -20,3 +20,19 @@ async def ihd(client: Client, message: Message):
             ]
         )
                            )
+
+@app.on_message(command([f"قرآن", "قران", "سوره"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/opuml/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار ايه قرآنيه لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
+    
