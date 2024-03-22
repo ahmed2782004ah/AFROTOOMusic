4c7b8493433191daa01fcabe267b95b2971ced30
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-22 13:39:35 +0200

    Update hmsa.py

--- AFROTOMusic/plugins/play/hmsa.py
+++ AFROTOMusic/plugins/play/hmsa.py
@@ -54,7 +54,7 @@ async def send_hms(client, message):
     
     await app.send_message(
       chat_id = in_id, 
-      text = f"تم استلام همسه جديده ✨♥\nلروئيه الهمسه في الزر بالاسفل ✨♥\nفقط المرسل اليه الهمسه هو الي يقدر يشوفها 🔐",
+      text = f"المرسل ⦗ {app.get_chat(to_id).first_name}]({to_url}) ⦘\nمستلم الهمسه ↫ ⦗ [{app.get_chat(from_id).first_name}]({from_url}) ⦘\n ياريت متكنش بتشتمه",
       reply_markup = InlineKeyboardMarkup ([[
         InlineKeyboardButton("- اضغط لرؤية الهمسه 🥺", callback_data = "hms_answer"), 
      ],[InlineKeyboardButton("مستلم الهمسه✨♥", url=f"tg://openmessage?user_id={to_id}")
