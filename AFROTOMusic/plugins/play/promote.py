eff672033a3821eb72c80a8ff36aafcc91139747
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-26 03:53:50 +0200

    Update promote.py

--- AFROTOMusic/plugins/play/promote.py
+++ AFROTOMusic/plugins/play/promote.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, ChatPermissions
 from AFROTOMusic import app
+from pyrogram.enums import ChatMemberStatus
 
 async def PROMOTE_OWNER(c:Client,m:Message):
     ChatID = m.chat.id
