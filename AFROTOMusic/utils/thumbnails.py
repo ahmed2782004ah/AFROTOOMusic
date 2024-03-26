582fedb839b3374c0c271289b3b0706838a7d61f
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-26 14:21:08 +0200

    Update thumbnails.py

--- AFROTOMusic/utils/thumbnails.py
+++ AFROTOMusic/utils/thumbnails.py
@@ -115,15 +115,15 @@ async def get_thumb(videoid):
         )   
         
         title1 = truncate(title)
-            image4.text(
-                (670, 300),
-                text=title1[0],
-                fill="white",
-                stroke_width=3,
-                stroke_fill="white",
-                font=font3,
-                align="left",
-            )
+        image4.text(
+               (670, 300),
+               text=title1[0],
+               fill="white",
+               stroke_width=3,
+               stroke_fill="white",
+               font=font3,
+               align="left",
+           )
             image4.text(
                 (670, 350),
                 text=title1[1],
