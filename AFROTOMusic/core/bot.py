bb3d3fe2333f9c38601787c28b734b3de6eeeb18
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-05-06 03:57:53 +0300

    Update bot.py

--- AFROTOMusic/core/bot.py
+++ AFROTOMusic/core/bot.py
@@ -1,12 +1,14 @@
-from pyrogram.enums import ParseMode
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
 
-from AFROTOMusic import app
-from AFROTOMusic.utils.database import is_on_off
-from config import OWNER_ID
+import config
+
+from ..logging import LOGGER
+
 
 class Zelzaly(Client):
     def __init__(self):
-        OWNER_ID("ميــوزك عفرتو").info(f"جارِ بدء تشغيل البوت . . .")
+        LOGGER("ميــوزك عفرتو").info(f"جارِ بدء تشغيل البوت . . .")
         super().__init__(
             name="AFROTOMusic",
             api_id=config.API_ID,
@@ -26,9 +28,25 @@ class Zelzaly(Client):
 
         try:
             await self.send_message(
-                chat_id=config.OWNER_ID,
+                chat_id=config.LOGGER_ID,
                 text=f"<u><b>» تم تشغيل الميـوزك لـ البوت {self.mention} :</b><u>\n\n- ɪᴅ : <code>{self.id}</code>\n- ɴᴀᴍᴇ : {self.name}\n- ᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-                        )
+            )
+        except (errors.ChannelInvalid, errors.PeerIdInvalid):
+            LOGGER(__name__).error(
+                "» قم باضافة البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
+            )
+            exit()
+        except Exception as ex:
+            LOGGER(__name__).error(
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+            )
+            exit()
+
+        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(__name__).error(
+                "» قم برفـع البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
+            )
             exit()
         LOGGER("ميــوزك عفرتو").info(f" تم بدء تشغيل البوت {self.name} ...✓")
 
