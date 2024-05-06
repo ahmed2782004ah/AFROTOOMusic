ad3d17b2a1c1ce5ea20d4d74137dc0dd819c90eb
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-05-07 02:20:18 +0300

    Update start.py

--- AFROTOMusic/plugins/bot/start.py
+++ AFROTOMusic/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                video=config.START_IMG_URL,
+                photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHANNEL),
                 reply_markup=keyboard,
             )
@@ -74,7 +74,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            video=config.START_IMG_URL,
+            photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -91,7 +91,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        video=config.START_IMG_URL,
+        photo=config.START_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -126,7 +126,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    video=config.START_IMG_URL,
+                    photo=config.START_IMG_URL,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
