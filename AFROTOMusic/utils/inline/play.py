502dcd2441706916475f29d75be47382cf242bcb
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-06-05 03:02:08 +0300

    Update play.py

--- AFROTOMusic/utils/inline/play.py
+++ AFROTOMusic/utils/inline/play.py
@@ -1,10 +1,8 @@
 import math
-from config import *
-import config
+from config import SUPPORT_CHAT, OWNER_ID
 from pyrogram.types import InlineKeyboardButton
 
 from AFROTOMusic.utils.formatters import time_to_seconds
-
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -28,70 +26,63 @@ def track_markup(_, videoid, user_id, channel, fplay):
     ]
     return buttons
 
+
 def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "⧂━━━━━━━━━━━━━"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "──⧂━━━━━━━━━━━"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "───⧂━━━━━━━━━━"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "────⧂━━━━━━━━━"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "─────⧂━━━━━━━━"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "──────⧂━━━━━━━"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "────────⧂━━━━━"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "──────────⧂━━━"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "───────────⧂━━"
+        bar = "————————◉—"
     else:
-        bar = "─────────────⧂"
+        bar = "—————————◉"
     buttons = [
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],[
+        ],
+        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-        [
-        InlineKeyboardButton(
-                 "ᴀғʀᴏᴛᴏᴏ", url=SUPPORT_CHANNEL
-            ),
-        ],
-        [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-        ],
+        
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
+
 
 def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="▷▷", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-        [
-        InlineKeyboardButton(
-                 "ᴀғʀᴏᴛᴏᴏ", url=SUPPORT_CHANNEL
-            ),
-        ],
-        [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-        ],
+            
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -101,11 +92,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
