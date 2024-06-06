8c75d88f7dee08221988126639067a1677c4a46f
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-06-06 23:06:55 +0300

    Update groupedit.py

--- AFROTOMusic/plugins/play/groupedit.py
+++ AFROTOMusic/plugins/play/groupedit.py
@@ -1,10 +1,10 @@
 from pyrogram import enums
 from pyrogram.enums import ChatType
 from pyrogram import filters, Client
-from AarohiX import app
+from AFROTOMusic import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from AarohiX.utils.admin_check import admin_filter
+from AFROTOMusic.utils.admin_check import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 # ------------------------------------------------------------------------------- #
