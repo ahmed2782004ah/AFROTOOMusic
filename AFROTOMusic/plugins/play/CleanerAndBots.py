f6f3839d6eac2a054a4e2857b0213cda3818a67d
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-06-07 00:56:54 +0300

    Update CleanerAndBots.py

--- AFROTOMusic/plugins/play/CleanerAndBots.py
+++ AFROTOMusic/plugins/play/CleanerAndBots.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 from pyrogram import enums
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait
-from AarohiX import app
+from AFROTOMusic import app
 
 # ------------------------------------------------------------------------------- #
 
